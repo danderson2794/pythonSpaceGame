6f1ee00d45aa15589589ca271785e7b255100628
root <root@DESKTOP-N2K98DB.localdomain>
2018-07-08 20:45:37 -0400

    Implemented the firing mechanism. Need to come back and debug later.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,34 +1,35 @@
 import pygame
 
 from settings import Settings
-
+from ship import Ship
+from pygame.sprite import Group 
+import game_functions as gf
 
 def run_game():
-    # Initialize pygame, settings, and screen object.
-    pygame.init()
-    #below is screen resolution
-    ai_settings = Settings()
+        
     
+    ai_settings = Settings()
+    pygame.display.set_caption("Alien Invasion")
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height)
     )
-    
-    pygame.display.set_caption("Alien Invasion")
-
-
     #This sets the background color
     bg_color = (230, 230, 230)
 
-        #start the main loop for the game.
+    # Make a ship
+    ship = Ship(ai_settings, screen)
+
+    # Make a group to store bullets in
+    bullets = Group()
+
+    #start the main loop for the game.
     while True:
+        #screen.fill(ai_settings.bg_color)
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
-        # Redraw the screen during each pass through the loop
-        screen.fill(ai_settings.bg_color)
-        
-        #make the most recently drawn screen visible.
-        pygame.display.flip()
+       
 
-run_game()
-
-
-        
+run_game() 
